88008bb0c2b9b4725024c3bc9a9c2501fa2ed74f
Lucas Gomes Matos <lgomes.lg99@gmail.com>
2022-06-15 22:06:15 -0300

    import

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,5 @@
 import ScrollSuave from "./modules/scroll-suave.js";
 import initTabNav from "./modules/tabnav.js";
-import initAnimacaoScroll from "./modules/scroll-animacao.js";
 import initAccordionList from "./modules/accordion-list.js";
 import initTooltip from "./modules/tooltip.js";
 import initModal from "./modules/modal.js";
@@ -9,13 +8,13 @@ import initMenuMobile from "./modules/menu-mobile.js";
 import initFuncionamento from "./modules/funcionamento.js";
 import initFetchAnimais from "./modules/fetch-animais.js";
 import initFetchBitcoin from "./modules/fetch-bitcoin.js";
+import initAnimacaoScroll from "./modules/scroll-animacao.js";
 
 
 const scrollSuave = new ScrollSuave(`[data-menu="suave"] a[href^="#"]`);
 scrollSuave.init();
 
 initTabNav();
-initAnimacaoScroll();
 initAccordionList();
 initModal();
 initTooltip();
@@ -24,6 +23,7 @@ initMenuMobile();
 initFuncionamento();
 initFetchAnimais();
 initFetchBitcoin();
+initAnimacaoScroll();
 
 
 
