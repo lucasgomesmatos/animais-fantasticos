f72962bca8fd438aacb6e1a7429d215bef140a21
Lucas Gomes Matos <lgomes.lg99@gmail.com>
2022-06-21 08:57:03 -0300

    path from animais api

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   entry: './assets/js/script.js',
   output: {
-    path: path.resolve(__dirname, './dist'),
+    path: path.resolve(__dirname),
     filename: 'main.js',
   },
   module: {
